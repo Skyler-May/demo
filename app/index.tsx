406e11573c227523e68733d72391499819a90e6e
Skyler-May <meisongyong@gmail.com>
2025-06-11 00:07:43 +0800

    Initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,36 @@
+import useLunar from "@/components/useCountLunar";
+import React from "react";
 import { Text, View } from "react-native";
 
-export default function Index() {
+const DateTimeDisplay = () => {
+  const { gregorian, lunar, zodiac } = useLunar();
+
+  const zodiacAgeMap = {
+    鼠: [1, 13, 25, 37, 49],
+    牛: [2, 14, 26, 38],
+    虎: [3, 15, 27, 39],
+    兔: [4, 16, 28, 40],
+    龙: [5, 17, 29, 41],
+    蛇: [6, 18, 30, 42],
+    马: [7, 19, 31, 43],
+    羊: [8, 20, 32, 44],
+    猴: [9, 21, 33, 45],
+    鸡: [10, 22, 34, 46],
+    狗: [11, 23, 35, 47],
+    猪: [12, 24, 36, 48],
+  };
+
+  do () {
+while (zodiacAgeMap === zodiac)
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text>{gregorian}</Text>
+      <Text>{lunar}</Text>
+      <Text>{zodiac}</Text>
     </View>
   );
-}
+};
+
+export default DateTimeDisplay;
