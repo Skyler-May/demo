1aa604e89f9bd70add9e6a89e6e2dc44b3298248
Skyler-May <meisongyong@gmail.com>
2025-06-11 18:15:20 +0800

    Initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
 import useLunar from "@/components/useCountLunar";
+import useZodiacAge from "@/components/useZodiacAge";
+
 import React from "react";
 import { Text, View } from "react-native";
 
 const DateTimeDisplay = () => {
   const { gregorian, lunar, zodiac } = useLunar();
+  const { mouseAges } = useZodiacAge();
 
   return (
     <View>
       <Text>{gregorian}</Text>
       <Text>{lunar}</Text>
       <Text>{zodiac}</Text>
+      <Text>{mouseAges} </Text>
     </View>
   );
 };
