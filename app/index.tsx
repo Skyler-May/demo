2e7f5c74fabb9fe00d2e274ae804dbd4866249d5
Skyler-May <meisongyong@gmail.com>
2025-06-11 18:24:12 +0800

    Initial commit

--- app/index.tsx
+++ app/index.tsx
@@ -4,18 +4,18 @@ import useZodiacAge from "@/components/useZodiacAge";
 import React from "react";
 import { Text, View } from "react-native";
 
-const DateTimeDisplay = () => {
+const Index = () => {
   const { gregorian, lunar, zodiac } = useLunar();
-  const { mouseAges } = useZodiacAge();
+  const { zodiacList } = useZodiacAge();
 
   return (
     <View>
-      <Text>{gregorian}</Text>
-      <Text>{lunar}</Text>
-      <Text>{zodiac}</Text>
-      <Text>{mouseAges} </Text>
+      <Text>当前日期：{gregorian}</Text>
+      <Text>当前农历日期：{lunar}</Text>
+      <Text>当前农历生肖：{zodiac}</Text>
+      <Text>{zodiacList} </Text>
     </View>
   );
 };
 
-export default DateTimeDisplay;
+export default Index;
